688f21f8cfd3835e57897eb7a7e9ede3c19fefa7
Kamil Szamajda <kamil.szamajda.programista@gmail.com>
2022-10-16 15:04:33 +0200

    Add registered user to file

--- KsiazkaAdresowa.cpp
+++ KsiazkaAdresowa.cpp
@@ -1,11 +1,15 @@
 #include "KsiazkaAdresowa.h"
 
-void KsiazkaAdresowa::rejestracjaUzytkownika(){
+KsiazkaAdresowa::KsiazkaAdresowa() {
+    nazwaPlikuZUzytkownikami = "Uzytkownicy.txt";
+}
+
+void KsiazkaAdresowa::rejestracjaUzytkownika() {
 
     Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
 
     uzytkownicy.push_back(uzytkownik);
-    //dopiszUzytkownikaDoPliku(uzytkownik);
+    dopiszUzytkownikaDoPliku(uzytkownik);
 
     cout << endl << "Konto zalozono pomyslnie" << endl << endl;
     system("pause");
@@ -19,13 +23,11 @@ Uzytkownik KsiazkaAdresowa::podajDaneNowegoUzytkownika() {
     uzytkownik.ustawId(pobierzIdNowegoUzytkownika());
 
     string login;
-    do
-    {
+    do {
         cout << "Podaj login: ";
         cin >> login;
         uzytkownik.ustawLogin(login);
-    }
-    while (czyIstniejeLogin(uzytkownik.pobierzLogin()) == true);
+    } while (czyIstniejeLogin(uzytkownik.pobierzLogin()) == true);
 
     string haslo;
     cout << "Podaj haslo: ";
@@ -43,8 +45,8 @@ int KsiazkaAdresowa::pobierzIdNowegoUzytkownika() {
 }
 
 bool KsiazkaAdresowa::czyIstniejeLogin(string login) {
-    for (int i = 0; i < uzytkownicy.size(); i++) {
-        if (uzytkownicy[i].pobierzLogin() == login){
+    for (unsigned int i = 0; i < uzytkownicy.size(); i++) {
+        if (uzytkownicy[i].pobierzLogin() == login) {
             cout << endl << "Istnieje uzytkownik o takim loginie." << endl;
             return true;
         }
@@ -52,10 +54,53 @@ bool KsiazkaAdresowa::czyIstniejeLogin(string login) {
     return false;
 }
 
-void KsiazkaAdresowa::wypiszWszystkichUzytkownikow(){
-    for (int i = 0; i < uzytkownicy.size(); i++) {
+void KsiazkaAdresowa::wypiszWszystkichUzytkownikow() {
+    for (unsigned int i = 0; i < uzytkownicy.size(); i++) {
         cout << uzytkownicy[i].pobierzId() << endl;
         cout << uzytkownicy[i].pobierzLogin() << endl;
         cout << uzytkownicy[i].pobierzHaslo() << endl;
     }
+}
+
+void KsiazkaAdresowa::dopiszUzytkownikaDoPliku (Uzytkownik uzytkownik) {
+    fstream plikTekstowy;
+    string liniaZDanymiUzytkownika = "";
+    plikTekstowy.open(nazwaPlikuZUzytkownikami.c_str(), ios::app);
+
+    if (plikTekstowy.good() == true) {
+        liniaZDanymiUzytkownika = zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(uzytkownik);
+
+        if (czyPlikJestPusty(plikTekstowy) == true) {
+            plikTekstowy << liniaZDanymiUzytkownika;
+        } else {
+            plikTekstowy << endl << liniaZDanymiUzytkownika ;
+        }
+    } else
+        cout << "Nie udalo sie otworzyc pliku " << nazwaPlikuZUzytkownikami << " i zapisac w nim danych." << endl;
+    plikTekstowy.close();
+}
+
+string KsiazkaAdresowa::zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik) {
+    string liniaZDanymiUzytkownika = "";
+
+    liniaZDanymiUzytkownika += konwerjsaIntNaString(uzytkownik.pobierzId())+ '|';
+    liniaZDanymiUzytkownika += uzytkownik.pobierzLogin() + '|';
+    liniaZDanymiUzytkownika += uzytkownik.pobierzHaslo() + '|';
+
+    return liniaZDanymiUzytkownika;
+}
+
+string KsiazkaAdresowa::konwerjsaIntNaString(int liczba) {
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
+}
+
+bool KsiazkaAdresowa::czyPlikJestPusty(fstream &plikTekstowy) {
+    plikTekstowy.seekg(0, ios::end);
+    if (plikTekstowy.tellg() == 0)
+        return true;
+    else
+        return false;
 }
--- Uzytkownik.cpp
+++ Uzytkownik.cpp
@@ -1,27 +1,27 @@
 #include "Uzytkownik.h"
 
-    void Uzytkownik::ustawId (int noweId){
-        if (noweId >= 0)
+void Uzytkownik::ustawId (int noweId) {
+    if (noweId >= 0)
         id = noweId;
-    }
-    void Uzytkownik::ustawLogin (string nowyLogin){
+}
+void Uzytkownik::ustawLogin (string nowyLogin) {
 
-        login = nowyLogin;
-    }
-    void Uzytkownik::ustawHaslo (string noweHaslo){
+    login = nowyLogin;
+}
+void Uzytkownik::ustawHaslo (string noweHaslo) {
 
-        haslo = noweHaslo;
-    }
+    haslo = noweHaslo;
+}
 
-    int Uzytkownik::pobierzId(){
+int Uzytkownik::pobierzId() {
 
-        return id;
-    }
-    string Uzytkownik::pobierzLogin(){
+    return id;
+}
+string Uzytkownik::pobierzLogin() {
 
-        return login;
-    }
-    string Uzytkownik::pobierzHaslo() {
+    return login;
+}
+string Uzytkownik::pobierzHaslo() {
 
-        return haslo;
-    }
+    return haslo;
+}
