fe4d6c35f439be447d393bc891d681d58af18b6d
KamilSz <kamil.szamajda.programista@gmail.com>
2025-01-31 08:43:23 +0100

    New class Adresat added

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -11,6 +11,7 @@ using namespace std;
 class KsiazkaAdresowa {
 
     UzytkownikMenedzer uzytkownikMenedzer;
+    //AdresatMenedzer adresatMenedzer;
 
 public:
     KsiazkaAdresowa(string nazwaPlikuZUzytkownikami);
